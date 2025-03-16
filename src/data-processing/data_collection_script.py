15fc10ba88dead2b720f4b7969f07c5c92cb8b65
mukund-setti <117050163+mukund-setti@users.noreply.github.com>
2025-03-16 10:59:12 -0700

    Data collection modified

--- src/data-processing/data_collection_script.py
+++ src/data-processing/data_collection_script.py
@@ -1,6 +1,6 @@
 import csv
 
-# Read multiple CSV files and create a cumulative dictionary
+# Function to read multiple CSV files and create a cumulative dictionary
 def read_images_from_csv(files):
     images_by_label = {}
 
@@ -8,28 +8,31 @@ def read_images_from_csv(files):
         with open(csv_file, 'r') as file:
             reader = csv.reader(file)
             for row in reader:
-                # Get image path and label
+                #get image path and label
                 _, image_name, label = row
-
-                # Standardize titles in case of capitalization
+                #standardize titles in case of capitlization
                 label = label.lower()
                 if label not in images_by_label and label != 'emotion':
-                    # Merge sad into sadness label
+                    #merge sad into sadness label
                     if label == 'sad':
                         images_by_label['sadness'] = []
+                    elif label == 'contempt':
+                        continue
                     else:
                         images_by_label[label] = []
-
-                # Exclude header line
+                #exclude header line
                 if label != 'emotion':
+                    #merge cont.
                     if label == 'sad':
                         images_by_label['sadness'].append(image_name)
+                    elif label == 'contempt':
+                        continue
                     else:
                         images_by_label[label].append(image_name)
 
     return images_by_label
 
-csv_files = ["/home/kyns/Desktop/Code/Classes/178/ClassProject/rsrc/facial_expressions/data/500_picts_satz.csv", "/home/kyns/Desktop/Code/Classes/178/ClassProject/rsrc/facial_expressions/data/legend.csv"]
+csv_files = ["C:\\Users\\mukun\\CS 178\\Emotions CNN Project\\data\\500_picts_satz.csv", "C:\\Users\\mukun\\CS 178\\Emotions CNN Project\\data\\legend.csv"]
 cumulative_images = read_images_from_csv(csv_files)
 
 # Print the merged dictionary
